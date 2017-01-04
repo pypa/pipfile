ba529f6e7badf1c75583f3de755afc3ceb79faa5
Kenneth Reitz <me@kennethreitz.org>
2017-01-04 00:47:04 -0500

    rest of the 508 specifiers

--- pipfile/api.py
+++ pipfile/api.py
@@ -1,7 +1,23 @@
 import json
 import hashlib
 import platform
+import sys
+import os
 from collections import OrderedDict
+
+
+def format_full_version(info):
+    version = '{0.major}.{0.minor}.{0.micro}'.format(info)
+    kind = info.releaselevel
+    if kind != 'final':
+        version += kind[0] + str(info.serial)
+    return version
+
+
+if hasattr(sys, 'implementation'):
+    implementation_version = format_full_version(sys.implementation.version)
+else:
+    implementation_version = "0"
 
 
 class PipfileParser(object):
@@ -108,12 +124,28 @@ class Pipfile(object):
         return json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
 
     def assert_requirements(self):
+
+        lookup = {
+            'os_name': os.name,
+            'sys_platform': sys.platform,
+            'platform_machine': platform.machine(),
+            'platform_python_implementation': platform.python_implementation(),
+            'platform_release': platform.release(),
+            'platform_system': platform.system(),
+            'platform_version': platform.version(),
+            'python_version': platform.python_version()[:3],
+            'python_full_version': platform.python_version(),
+            # 'implementation_name': sys.implementation.name,
+            'implementation_version': implementation_version
+
+        }
+
         for requirement in self.data['_meta']['requires']:
             marker = requirement['marker']
             specifier = requirement['specifier']
 
-            if marker == 'python_version':
-                assert platform.python_version()[:3] == specifier
+            if marker in lookup:
+                assert lookup[marker] == specifier
 
 
 def load(pipfile_path):
